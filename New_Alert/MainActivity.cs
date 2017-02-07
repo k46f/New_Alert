8152af61f269f7cb582da67256e65b6d7878c962
John Ramirez <k46f@hotmail.com>
2017-02-08 00:12:23 +0800

    First Commit

---
+++ New_Alert/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace New_Alert
+{
+    [Activity(Label = "New_Alert", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            Button SButton = FindViewById<Button>(Resource.Id.button1);
+            TextView TText = FindViewById<TextView>(Resource.Id.textView1);
+
+
+            SButton.Click += delegate {
+
+                var AName = (EditText)FindViewById(Resource.Id.editText1);
+                var ALName = (EditText)FindViewById(Resource.Id.editText2);
+
+                String Name = AName.Text;
+                String LName = ALName.Text;
+
+                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                alert.SetTitle("Hey! " + Name);
+                alert.SetMessage("Hi! " + Name + LName);
+
+                alert.SetNegativeButton("Back", (senderAlert, args) => {
+                    
+                });
+
+                Dialog dialog = alert.Create();
+                dialog.Show();
+            };
+
+
+        }
+    }
+}
+
